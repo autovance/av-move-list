05a1999c8b64d4c2b962bcd9e70fc1f4f9381c72
Chris Rabl <rabl.christopher@gmail.com>
2015-10-23 11:35:27 -0600

    Updated with IIFE

--- av-move-list.js
+++ av-move-list.js
@@ -20,110 +20,112 @@ TODO
 //end: usage
 */
 
-'use strict';
+(function () {
+  'use strict';
 
-angular
-  .module('autovance.av-move-list', [])
-  .directive('avMoveList', ['$q', MoveListDirective]);
+  angular
+    .module('autovance.av-move-list', [])
+    .directive('avMoveList', ['$q', MoveListDirective]);
 
-function MoveListDirective($q) {
-  return {
-    restrict: 'EA',
-    require: 'ngModel',
-    scope: {
-      selectedLabel: '@',
-      availableLabel: '@',
-      displayAttr: '@',
-			idAttr: '@',
-      all: '=',
-      model: '=ngModel'
-    },
+  function MoveListDirective($q) {
+    return {
+      restrict: 'EA',
+      require: 'ngModel',
+      scope: {
+        selectedLabel: '@',
+        availableLabel: '@',
+        displayAttr: '@',
+        idAttr: '@',
+        all: '=',
+        model: '=ngModel'
+      },
 
-    template:
-    '<div class="multiSelect">' +
-      '<div class="select">' +
-        '<label class="control-label" for="multiSelectSelected">{{ selectedLabel }} ' +
-            '({{ model.length }})</label>' +
-        '<select id="currentRoles" ng-model="selected.current" multiple ' +
-            'class="pull-left" ng-options="e as e[displayAttr] for e in model">' +
-            '</select>' +
-      '</div>' +
-      '<div class="select buttons">' +
-        '<button class="btn mover left" ng-click="add()" title="Add selected" ' +
-            'ng-disabled="selected.available.length == 0">' +
-          '<i class="glyphicon glyphicon-arrow-left"></i>' +
-        '</button>' +
-        '<button class="btn mover right" ng-click="remove()" title="Remove selected" ' +
-            'ng-disabled="selected.current.length == 0">' +
-          '<i class="glyphicon glyphicon-arrow-right"></i>' +
-        '</button>' +
-      '</div>' +
-      '<div class="select">' +
-        '<label class="control-label" for="multiSelectAvailable">{{ availableLabel }} ' +
-            '({{ available.length }})</label>' +
-        '<select id="multiSelectAvailable" ng-model="selected.available" multiple ' +
-            'ng-options="e as e[displayAttr] for e in available"></select>' +
-      '</div>' +
-    '</div>',
+      template:
+      '<div class="multiSelect">' +
+        '<div class="select">' +
+          '<label class="control-label" for="multiSelectSelected">{{ selectedLabel }} ' +
+              '({{ model.length }})</label>' +
+          '<select id="currentRoles" ng-model="selected.current" multiple ' +
+              'class="pull-left" ng-options="e as e[displayAttr] for e in model">' +
+              '</select>' +
+        '</div>' +
+        '<div class="select buttons">' +
+          '<button class="btn mover left" ng-click="add()" title="Add selected" ' +
+              'ng-disabled="selected.available.length == 0">' +
+            '<i class="glyphicon glyphicon-arrow-left"></i>' +
+          '</button>' +
+          '<button class="btn mover right" ng-click="remove()" title="Remove selected" ' +
+              'ng-disabled="selected.current.length == 0">' +
+            '<i class="glyphicon glyphicon-arrow-right"></i>' +
+          '</button>' +
+        '</div>' +
+        '<div class="select">' +
+          '<label class="control-label" for="multiSelectAvailable">{{ availableLabel }} ' +
+              '({{ available.length }})</label>' +
+          '<select id="multiSelectAvailable" ng-model="selected.available" multiple ' +
+              'ng-options="e as e[displayAttr] for e in available"></select>' +
+        '</div>' +
+      '</div>',
 
-    link: function(scope, elm, attrs) {
-      scope.selected = {
-        available: [],
-        current: []
-      };
+      link: function(scope, elm, attrs) {
+        scope.selected = {
+          available: [],
+          current: []
+        };
 
-      // Filters out items in 'all' that are also in 'selected'. Compares by value.
-      function filterAvailable(all, selected) {
-        var available = all.filter(function (item) {
+        // Filters out items in 'all' that are also in 'selected'. Compares by value.
+        function filterAvailable(all, selected) {
+          var available = all.filter(function (item) {
 
-          // item is selected if it is first OR second, OR third OR ... in the array of selected items
-          var is_selected = selected.reduce(function (item_is_selected, selected_item) {
-            return item_is_selected || angular.equals(item, selected_item);
-          }, false);
+            // item is selected if it is first OR second, OR third OR ... in the array of selected items
+            var is_selected = selected.reduce(function (item_is_selected, selected_item) {
+              return item_is_selected || angular.equals(item, selected_item);
+            }, false);
 
-          // item is available if it is NOT selected
-          return !is_selected;
-        });
-
-        return available;
-      }
-
-      scope.refreshAvailable = function() {
-        scope.available = filterAvailable(scope.all, scope.model);
-        scope.selected.available = [];
-        scope.selected.current = [];
-      };
-
-      scope.add = function() {
-        scope.model = scope.model.concat(scope.selected.available);
-        scope.refreshAvailable();
-      };
-
-      scope.remove = function() {
-        scope.model = filterAvailable(scope.model, scope.selected.current);
-        scope.refreshAvailable();
-      };
-
-      // Handles case when scope data hasn't been initialized yet
-      var dataLoading = function(scopeAttr) {
-        var loading = $q.defer();
-
-        if(scope[scopeAttr]) {
-          loading.resolve(scope[scopeAttr]);
-        } else {
-          scope.$watch(scopeAttr, function(newValue, oldValue) {
-            if(newValue) {
-              loading.resolve(newValue);
-            }
+            // item is available if it is NOT selected
+            return !is_selected;
           });
+
+          return available;
         }
 
-        return loading.promise;
-      };
+        scope.refreshAvailable = function() {
+          scope.available = filterAvailable(scope.all, scope.model);
+          scope.selected.available = [];
+          scope.selected.current = [];
+        };
 
-      $q.all([dataLoading("model"), dataLoading("all")]).then(function (results) {
-        scope.refreshAvailable();
-      });
-    }
-  };
-}
+        scope.add = function() {
+          scope.model = scope.model.concat(scope.selected.available);
+          scope.refreshAvailable();
+        };
+
+        scope.remove = function() {
+          scope.model = filterAvailable(scope.model, scope.selected.current);
+          scope.refreshAvailable();
+        };
+
+        // Handles case when scope data hasn't been initialized yet
+        var dataLoading = function(scopeAttr) {
+          var loading = $q.defer();
+
+          if(scope[scopeAttr]) {
+            loading.resolve(scope[scopeAttr]);
+          } else {
+            scope.$watch(scopeAttr, function(newValue, oldValue) {
+              if(newValue) {
+                loading.resolve(newValue);
+              }
+            });
+          }
+
+          return loading.promise;
+        };
+
+        $q.all([dataLoading("model"), dataLoading("all")]).then(function (results) {
+          scope.refreshAvailable();
+        });
+      }
+    };
+  }
+})();
